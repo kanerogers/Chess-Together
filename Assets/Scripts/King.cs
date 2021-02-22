cf430a2b350962b6f79257d0ac7fdab326cb650b
Kane Rogers <kane.m.rogers@gmail.com>
2021-02-22 16:37:32 +1100

    First implementation of IsCastling

--- Assets/Scripts/King.cs
+++ Assets/Scripts/King.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f54186160e997ecc34ad1e768d31a5baee0531b790a3d06c45d34dbed2b95ee7
-size 1525
+oid sha256:cc8fcefd5f02a8d580d02707c1fda83cafc58f12b74b8587d631a2b6bba5ce98
+size 2865
