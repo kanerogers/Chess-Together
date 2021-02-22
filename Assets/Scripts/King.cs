946dfe7348f02b38668464f8634fc501b10f2009
Kane Rogers <kane.m.rogers@gmail.com>
2021-02-22 17:52:06 +1100

    castling wip

--- Assets/Scripts/King.cs
+++ Assets/Scripts/King.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc8fcefd5f02a8d580d02707c1fda83cafc58f12b74b8587d631a2b6bba5ce98
-size 2865
+oid sha256:a19989444e2b56bb8d3d8eccac0b9645120dbf1e2482d0b13b653fcda9cc11b0
+size 2484
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0540a8e2fa69b65d2ebbfacdafb2d73bffaa565b42f71dd079144dd5053fec7
-size 2030
+oid sha256:ccdb568d9426e388747220b0ed0af6515b895bdd3ccb9ef4fbbe957101588ea3
+size 2058
