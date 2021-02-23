be65dd4add75a8c192fb39cd7ff04d52dde38c12
Kane Rogers <kane.m.rogers@gmail.com>
2021-02-23 17:19:37 +1100

    wip castling - fixing tests

--- Assets/Scripts/SceneChessBoard.cs
+++ Assets/Scripts/SceneChessBoard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7216114a31186ee3e5e9e78cdf3bc27905aa16f2471341bb3d1c62bfcc385fa1
-size 6885
+oid sha256:88a7b874edb2d2e932e5d3011fc8af06824ad27f66dacc1e40d30e3452ebdf31
+size 7956
