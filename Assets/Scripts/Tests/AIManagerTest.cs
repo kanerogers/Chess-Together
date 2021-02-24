6517e374698997e17121bab89db8e330b0c8e128
Kane Rogers <kane.m.rogers@gmail.com>
2021-02-25 10:32:31 +1100

    Add support for stalemate. Closes #4

--- Assets/Scripts/Tests/AIManagerTest.cs
+++ Assets/Scripts/Tests/AIManagerTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cefcb63089b6c5b7dc9c9d2ff778cd9de904a799a786abb76db4ff2b429ea383
-size 2235
+oid sha256:ceb5b595bfcda83d8f4d168fc6f1e95bdedc34f73e4cb56c76f3f64335c1508c
+size 2486
