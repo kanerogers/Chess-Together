836ca5dd49b945fde530412bfd9a9cd647a29c58
Kane Rogers <kane.m.rogers@gmail.com>
2021-02-25 15:43:09 +1100

    No promotion Fixes #1

--- Assets/Scripts/ChessPiece.cs
+++ Assets/Scripts/ChessPiece.cs
@@ -1,3 +1,123 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f57b06eb4c582ece7028577e3874b151529322924cbb3ba84cf99a14132c9463
-size 3120
+﻿using System;
+
+public abstract class ChessPiece {
+    public EColour Colour;
+    public EName Name;
+    public int Row;
+    public int Column;
+    public bool HasMoved;
+
+    public virtual bool CheckMove(ChessPiece[,] pieces, int row, int column) {
+        if (row == Row && column == Column) {
+            return false;
+        }
+
+        if (row > 7 || row < 0) {
+            return false;
+        }
+        if (column > 7 || column < 0) {
+            // Logger.Log("Moves", "Invalid column " + column);
+            return false;
+        }
+
+        ChessPiece piece = pieces[row, column];
+
+        if (piece != null) {
+            // FIDE 3.1 It is not permitted to move a piece to a square occupied by a piece of the same colour.
+            if (piece.Colour == Colour) {
+                // Logger.Log("Moves", $"Unable to move {Name} to {row},{column}. This space is occupied by {piece}");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public abstract int GetScore();
+
+    public bool HasPiecesOnInterveningSquares(ChessPiece[,] pieces, int toRow, int toColumn) {
+        int rowDelta;
+        int colDelta;
+
+        if (toRow > Row) rowDelta = 1;
+        else if (toRow < Row) rowDelta = -1;
+        else rowDelta = 0;
+
+        if (toColumn > Column) colDelta = 1;
+        else if (toColumn < Column) colDelta = -1;
+        else colDelta = 0;
+
+        int row = Row + rowDelta;
+        int column = Column + colDelta;
+
+        // Case 1: Same row
+        if (rowDelta == 0) {
+            for (; column != toColumn; column += colDelta) {
+                var piece = pieces[Row, column];
+                if (piece != null) return true;
+            }
+            return false;
+        }
+
+        // Case 2: Same Column
+        if (colDelta == 0) {
+            for (; row != toRow; row += rowDelta) {
+                var piece = pieces[row, Column];
+                if (piece != null) return true;
+            }
+            return false;
+        }
+
+
+        // Case 3: Different row and column
+        for (; row != toRow && column != toColumn; row += rowDelta, column += colDelta) {
+            var piece = pieces[row, column];
+            if (piece != null) return true;
+        }
+
+        return false;
+    }
+
+
+    public bool IsDiagonal(int toRow, int toColumn) {
+        // Use Baby Chen (2020) Formula
+        var a = Math.Abs(toRow - Row);
+        var b = Math.Abs(toColumn - Column);
+        return a == b;
+    }
+    public bool IsRankOrFile(int toRow, int toColumn) {
+        return (Row == toRow || Column == toColumn);
+    }
+
+    public bool IsBlack() {
+        return Colour == ChessPiece.EColour.Black;
+    }
+
+    public bool IsWhite() {
+        return Colour == ChessPiece.EColour.Black;
+    }
+
+
+    public override string ToString() {
+        return $"{Colour} {Name} at {Row},{Column}";
+    }
+
+    public ChessPiece Clone() {
+        return (ChessPiece)this.MemberwiseClone();
+    }
+
+    public enum EColour {
+        Black, White
+    }
+
+    public enum EName {
+        None, // Annoyingly necessary because of Move.PieceToPromoteTo
+        Rook,
+        Knight,
+        King,
+        Queen,
+        Pawn,
+        Bishop
+    }
+
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,3 +1,82 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2743dfa5dff33584f5f4c735f8ad310dd1ddfb0d4310acd56bb5805d988e1aa4
-size 2086
+using System;
+using System.Collections.Generic;
+
+public class Move : IEquatable<Move> {
+    public int Sequence;
+    public int FromRow;
+    public int FromColumn;
+    public int ToRow;
+    public int ToColumn;
+    public int Score;
+    public string Player;
+    public bool isCastling;
+    public bool firstMoved;
+    public ChessPiece.EName PieceToPromoteTo;
+
+    public Move(int fromRow, int fromColumn, int toRow, int toColumn) {
+        FromRow = fromRow;
+        FromColumn = fromColumn;
+        ToRow = toRow;
+        ToColumn = toColumn;
+    }
+
+    public Move(int fromRow, int fromColumn, int toRow, int toColumn, ChessPiece.EName pieceToPromoteTo) {
+        FromRow = fromRow;
+        FromColumn = fromColumn;
+        ToRow = toRow;
+        ToColumn = toColumn;
+        PieceToPromoteTo = pieceToPromoteTo;
+    }
+
+    public Move(int fromRow, int fromColumn, int toRow, int toColumn, int score) {
+        FromRow = fromRow;
+        FromColumn = fromColumn;
+        ToRow = toRow;
+        ToColumn = toColumn;
+        Score = score;
+    }
+
+    public Move(int fromRow, int fromColumn, int toRow, int toColumn, int sequence, string player) {
+        FromRow = fromRow;
+        FromColumn = fromColumn;
+        ToRow = toRow;
+        ToColumn = toColumn;
+        Sequence = sequence;
+        Player = player;
+    }
+
+    public override string ToString() {
+        if (PieceToPromoteTo == 0) {
+            return $"Move {Sequence}: {FromRow},{FromColumn} to {ToRow},{ToColumn}. Score: {Score}";
+        } else {
+            return $"Move {Sequence}: {FromRow},{FromColumn} to {ToRow},{ToColumn}. Score: {Score}, Promote to: {PieceToPromoteTo}";
+        }
+    }
+
+    public Move(object obj) {
+        var source = (IDictionary<string, object>)obj;
+
+        FromRow = (int)(long)source["FromRow"];
+        FromColumn = (int)(long)source["FromColumn"];
+        ToRow = (int)(long)source["ToRow"];
+        ToColumn = (int)(long)source["ToColumn"];
+        Player = (string)source["Player"];
+        Sequence = (int)(long)source["Sequence"];
+    }
+
+    public Dictionary<string, object> ToDictionary() {
+        var d = new Dictionary<string, object>();
+        d.Add("FromRow", FromRow);
+        d.Add("FromColumn", FromColumn);
+        d.Add("ToRow", ToRow);
+        d.Add("ToColumn", ToColumn);
+        d.Add("Player", Player);
+        d.Add("Sequence", Sequence);
+        return d;
+    }
+
+    public bool Equals(Move move) {
+        if (move == null) return false;
+        return (move.ToRow == ToRow && move.ToColumn == ToColumn && move.FromRow == FromRow && move.FromColumn == FromColumn);
+    }
+}
