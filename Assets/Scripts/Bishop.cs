279d8c24893b7c15774dac4cb853647a26c12e0d
Kane Rogers <kane.m.rogers@gmail.com>
2021-03-03 13:35:18 +1100

    No promotion Fixes #1

--- Assets/Scripts/Bishop.cs
+++ Assets/Scripts/Bishop.cs
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5bb5592af5ffa6e3ffa5f95b1e942213eb2b431c300060b81f399a391ec8e216
-size 1203
+public class Bishop : ChessPiece {
+    public Bishop(EColour colour, int row, int column) {
+        Colour = colour;
+        Name = EName.Bishop;
+        Row = row;
+        Column = column;
+    }
+
+    public override bool CheckMove(ChessPiece[,] pieces, Move move) {
+        var (_, _, toRow, toColumn) = move.ToCoordinates();
+        if (!base.CheckMove(pieces, move)) {
+            // Logger.Log("Moves", $"Unable to move Bishop from {Row},{Column} to {toRow},{toColumn} - CheckMove failed");
+            return false;
+        }
+
+        // FIDE 3.5 "[...] the bishop [...] may not move over any intervening pieces."
+        if (HasPiecesOnInterveningSquares(pieces, toRow, toColumn)) {
+            // Logger.Log("Moves", $"Unable to move Bishop from {Row},{Column} to {toRow},{toColumn} - pieces in the way");
+            return false;
+        }
+
+        // FIDE 3.2 "The bishop may move to any square along a diagonal on which it stands."
+        if (!IsDiagonal(toRow, toColumn)) {
+            // Logger.Log("Moves", $"Unable to move Bishop from {Row},{Column} to {toRow},{toColumn} - non diagonal move");
+            return false;
+        }
+
+        return true;
+    }
+
+    public override int GetScore() {
+        return 3;
+    }
+}
