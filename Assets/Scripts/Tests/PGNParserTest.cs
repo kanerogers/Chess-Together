e4701b6763e6539d463358c6eb1f0a667247a4cd
Kane Rogers <kane.m.rogers@gmail.com>
2021-04-01 14:48:31 +1100

    wip FFFFF

--- Assets/Scripts/Tests/PGNParserTest.cs
+++ Assets/Scripts/Tests/PGNParserTest.cs
@@ -8,21 +8,25 @@ namespace Tests {
             var games = parser.Parse(1);
             Assert.AreEqual(1, games.Count);
             var moves = games[0];
-            Assert.AreEqual(85, moves.Count);
+            Assert.AreEqual(71, moves.Count);
         }
 
         [Test]
         public void ReadAll() {
             var parser = new PGNParser("Assets\\Scripts\\Tests\\test.pgn");
-            var games = parser.Parse();
-            Assert.AreEqual(3252, games.Count);
+            var games = parser.Parse(1);
+            // Assert.AreEqual(3252, games.Count);
             ChessBoard board = null;
             var gameIndex = 0;
+            var moveIndex = 0;
             try {
                 foreach (var game in games) {
                     board = new ChessBoard();
                     var canMove = ChessPiece.EColour.White;
                     foreach (var move in game) {
+                        if (moveIndex == 38) {
+                            Logger.AT_CORRECT_MOVE = true;
+                        }
                         // Make move
                         board.Move(move);
 
@@ -32,8 +36,21 @@ namespace Tests {
                         // Make move
                         board.Move(move);
 
-                        // Make the AI think
-                        AIManager.GetMove(board, canMove, AIManager.MoveType.Standard);
+                        var blackState = board.State[ChessPiece.EColour.Black];
+                        var whiteState = board.State[ChessPiece.EColour.White];
+                        if (blackState == ChessBoard.BoardStatus.Checkmate || blackState == ChessBoard.BoardStatus.Stalemate || whiteState == ChessBoard.BoardStatus.Checkmate || whiteState == ChessBoard.BoardStatus.Stalemate) {
+                            continue;
+                        }
+
+                        if (board.ValidMoves[canMove].Count > 0) {
+                            // Make the AI think
+                            AIManager.GetMove(board, canMove, AIManager.MoveType.Standard);
+                        }
+
+                        if (blackState == ChessBoard.BoardStatus.Checkmate || blackState == ChessBoard.BoardStatus.Stalemate || whiteState == ChessBoard.BoardStatus.Checkmate || whiteState == ChessBoard.BoardStatus.Stalemate) {
+                            Logger.Log("heya");
+                        }
+
 
                         // Play an AI move
                         var aiMove = AIManager.GetMove(board, canMove.Inverse(), AIManager.MoveType.Standard);
@@ -45,6 +62,7 @@ namespace Tests {
                         canMove = canMove.Inverse();
 
                         // Onto the next one..
+                        moveIndex += 1;
                     }
                     gameIndex += 1;
                 }
